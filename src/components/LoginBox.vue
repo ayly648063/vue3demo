b2b49e73bad56c0eb16c27630807d29a2a779c08
zzdate <13420069917@163.com>
2021-02-15 14:05:58 +0800

    change4

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>登录</div>
+  <el-row>
+  <el-col :xs="20" :sm="17" :md="14" :lg="11" :xl="8"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
+</template>
+
+<style lang="less" scoped>
+ .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+    margin: auto;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+     <LoginBox/>
+  </div>
+</template>
+<script>
+import LoginBox from '@/components/LoginBox'
+export default {
+  name: 'Login',
+  components: {
+    LoginBox
+  }
+}
+</script> 
